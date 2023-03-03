4df14d2cf485139d1e1bfce24f3d70c3d43a5ca9
ravindra-kumar-eng <117750035+ravindra-kumar-eng@users.noreply.github.com>
2023-03-03 12:24:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let pricesEl = document.getElementsByClassName('price');
+  let sum = 0;
+  for (let i = 0; i < pricesEl.length; i++) {
+    let x = Number(pricesEl[i].innerText);
+    sum += x;
+  }
+    // console.log(`TotalSum: ${sum}`);
+    const emptyRow = document.createElement("tr");
+    emptyRow.innerHTML = `
+    <td></td><td>${sum}</td>
+    `
+    // document.getElementById('tab').appendChild(emptyRow);
+    document.getElementById('tab').innerHTML += `<tr id="ans">
+    <td></td><td>${sum}</td>
+        </tr>`
 };
 
 getSumBtn.addEventListener("click", getSum);
